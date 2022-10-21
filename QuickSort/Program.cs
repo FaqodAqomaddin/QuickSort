239d899fbf77bf4d6688befd25069f65d42cf7a4
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-10-21 17:47:30 +0700

    Add Method display

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -90,5 +90,19 @@ namespace QuickSort
 
             q_sort(j + 1, high);
         }
+
+        void display()
+        {
+            Console.WriteLine("\n-------------------");
+            Console.WriteLine("Sorted array elements");
+            Console.WriteLine("---------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
+        }
     }
 }
