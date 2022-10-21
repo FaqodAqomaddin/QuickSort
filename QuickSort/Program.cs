b174d90802ba5d9bc68cdbeea6cf44612b55a9c4
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-10-21 17:13:40 +0700

    Add Method Input

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -13,5 +13,30 @@ namespace QuickSort
         private int mov_count = 0;
 
         private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+
+            for (int i = 0; i < n; i++);
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
