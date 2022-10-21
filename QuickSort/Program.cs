16249fdbc69473bf880a117ffdbc77a05bac3888
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-10-21 16:56:26 +0700

    Add Class and Array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -5,3 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace QuickSort
+{
+    class Program
+    {
+        private int[]arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        private int n;
+    }
+}
