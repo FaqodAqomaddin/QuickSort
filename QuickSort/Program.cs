149864901890b63370962ff8edd5bc79b2d14c4a
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-10-21 17:39:34 +0700

    Add Method q_sort

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -31,9 +31,9 @@ namespace QuickSort
             Console.WriteLine("Enter Array Elements");
             Console.WriteLine("\n==================");
 
-            for (int i = 0; i < n; i++);
+            for (int i = 0; i < n; i++)
             {
-                Console.Write("<" + (i + 1) + ">");
+                Console.Write("<" + (i+1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
@@ -46,6 +46,49 @@ namespace QuickSort
             temp = arr[x];
             arr[x] = arr[y];
             arr[y] = temp;
+        }
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                while ((arr[j] > pivot) && (j => low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j)
+                {
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
+
+            if (low < j)
+            {
+                swap(low, j);
+                mov_count++;
+            }
+            q_sort(low, j - 1);
+
+            q_sort(j + 1, high);
         }
     }
 }
